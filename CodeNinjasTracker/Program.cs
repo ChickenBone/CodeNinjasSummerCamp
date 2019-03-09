e44f60046cb15793aea073a5fd2516b085ff422a
Wyatt Whorton <bluebomber019@gmail.com>
2019-03-08 20:10:06 -0700

    Init

---
+++ CodeNinjasTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeNinjasTracker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
